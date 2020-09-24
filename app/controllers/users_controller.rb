fb16b8ec909d8a20fd3c9ee9581808402db80729
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-09-24 02:39:35 +0000

    Fixed Sign up problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,21 @@
 class UsersController < ApplicationController
-
   def new
   end
-
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    # user_params[:email] = user_params[:email].downcase.strip
+    puts " Hi this funct"
+    puts user_params
+    @user = User.new(user_params)
+    puts "Helloe #{@user}"
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/signup'
     end
   end
-
-    private
-
+  private
   def user_params
-    params.require(:user)
-          .permit(
-                  :first_name,
-                  :last_name, 
-                  :email, 
-                  :password, 
-                  :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
