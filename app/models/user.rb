e80d0007d790447f94e05c3344cc2165ba5fa376
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-09-24 02:50:14 +0000

    Minor syntax changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  def self.authenticate_with_credentials(user_email, password)
+		self.find_by(email: user_email.strip.downcase).try(:authenticate, password)
+	end
 end
